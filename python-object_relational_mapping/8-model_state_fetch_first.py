e237947f61fd35e1f9375141f914bdaf4a1d250d
Peter Johnson Odang <118651777+ChristianOdang@users.noreply.github.com>
2023-11-07 09:55:44 +0100

    Update 8-model_state_fetch_first

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,42 +1,25 @@
 '''
-A script that lists all state objects from the database hbtn_0e_6_usa 
+a script that prints the first State object from the database
 '''
-# Import Modules from ORM
-from sqlalchemy import create_engine, select
-import sqlalchemy
-from sqlalchemyy.ext.declerative import declarative_base
+if __name__ == "__main__":
+    from model_state import State, Base
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    import sys
 
-# Import Base and table from model
-from model_state import Base, State
+    path = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(path)
 
-# import sys to get values from terminal
-import sys
+    Base.metadata.create_all(engine)
 
-# import session for database interaction
-from sqlalchemy.orm import sessionmaker
-
-# define session for database interaction
-try:
-    # create a db engine connection
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}"
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    # create a session variable and bint the engine to it
     Session = sessionmaker(bind=engine)
-
-    # instatiate the session
     session = Session()
 
-    results = session.query(State).first()
-
-    if results is not None:
-        print(f"{results.id}: {results.name}")
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
-        print("Nothing")
+        print('Nothing')
 
-except AttributeError as e:
-    print(f"error message {e}")
-
-except sqlalchemy.exc.ProgrammingError as e:
-    print(f"An Error occured: {e}")
+    session.close()
