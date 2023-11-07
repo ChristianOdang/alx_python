569eea2c85ca8bdf8a3a64963432714b277340c7
Peter Johnson Odang <118651777+ChristianOdang@users.noreply.github.com>
2023-11-07 13:08:40 +0100

    Update 5-json_api.py

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,5 +1,6 @@
-""" 
-This is a Python script that takes the URL and send a request to the URL
+"""
+Python script that takes in a letter and sends a POST request to
+ http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 import sys
 import requests
@@ -8,14 +9,14 @@ if len(sys.argv) == 1:
     letter = ""
 else:
     letter = sys.argv[1]
-    
+
 parameter = {'q':letter}
-url = 'https://0.0.0.0:5000/search_user'
+url = 'http://0.0.0.0:5000/search_user'
 response = requests.post(url, data=parameter)
 
 try:
     if response.json():
-        print("[{}] {}".format(response.json()['id'], response.json()['name']))
+            print("[{}] {}".format(response.json()['id'], response.json()['name']))
     else:
         print("No result")
 except:
