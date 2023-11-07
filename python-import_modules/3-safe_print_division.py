d72ef06a1baf6e945bbbcb97f1390c608cfbf725
Peter Johnson Odang <118651777+ChristianOdang@users.noreply.github.com>
2023-11-07 13:21:08 +0100

    Update 3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,15 +1,11 @@
-# A function that divides 2 integers and print the result
-
+#!/usr/bin/python3
 def safe_print_division(a, b):
-  try:
-    result = a / b
-  except ZeroDivisionError:
-    print("Error: Cannot be divide by zero!")
-    return None
-  except Exception as e:
-    print("Error:", e)
-  finally:
-    print("Inside result: {}".format(result))
-    
-    return result
-  
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    else:
+        return result
+    finally:
+        print("Inside result: {}".format(result))
