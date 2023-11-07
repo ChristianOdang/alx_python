3d5fa2183e500bcfa223370ef963e82632908eb8
Peter Johnson Odang <118651777+ChristianOdang@users.noreply.github.com>
2023-11-07 14:43:23 +0100

    Update 2-inherits_from.py

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,22 +1,22 @@
-""" 
-This Module is issubclass method takes the obj type variable,
-and a_class method
+"""
+This module checks if an instance is directly or
+indirectly a n instance of specific class
+
 """
 
 
-def is_kind_class(obj, a_class):
-    """ 
-    Function to check if obj is an instance of class
-    Args:
-         obj ([type]): object
-         a_class ([type]): a class
-         
-    Returns:
-        True: obj ia an instance of a_class
-        False: obj not an instance of a a_class
-
+def inherits_from(obj, a_class):
     """
-    # if isinstance(type(obj), a_class)
+    check if obj is an instance of class
+    Args:
+        obj ([type]): object
+        a_class ([type]): a class
+
+    Returns:
+        True: obj is an instance of a_class
+        False: obj not an instance of a_class
+    """
+
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
