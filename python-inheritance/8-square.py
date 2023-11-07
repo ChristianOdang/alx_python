682b31973f1a6eee22801a3405b443c8ddff9db4
Peter Johnson Odang <118651777+ChristianOdang@users.noreply.github.com>
2023-11-07 14:51:06 +0100

    Update python project

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,23 +1,39 @@
-""" 
-Write a class Rectangle that inherit from BaseGeometry
+
 """
-Rectangle = __import__('-rectangle').Rectangle
+Module containing the Rectangle class.
+"""
+Rectangle = __import__('7-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ 
-    A class define for square that inherit Rectangle
     """
-
+    A class or blueprint that models a square
+    """
     def __init__(self, size):
-        """ 
-        Initialization method for class Square
         """
-        Rectangle.integer_validator(self, "size", size)
+        instantiate attributes to an object
+        """
+        self.integer_validator("size", size)
         self.__size = size
 
+    def __str__(self):
+        """
+        Returns a string representation of the Square instance.
+        Returns:
+            str: String representation of the Square instance.
+        """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+
     def area(self):
-        """ 
-        Method to calculate area
+        """
+        computes area of the square
         """
         return self.__size ** 2
