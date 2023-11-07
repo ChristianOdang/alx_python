bd5f1fb86546bfb64224261caaeaf25567203736
Peter Johnson Odang <118651777+ChristianOdang@users.noreply.github.com>
2023-11-07 13:19:30 +0100

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,14 @@
-import sys
-
-def main():
-  args = sys.argv[1:]
-  number_args = len(args)
-  
-  print("{} arguement{}:".format(number_args, 's' if number_args != 1 else ''))
-  for i, arg in enumerate(args, 1):
-    print("{}: {}".format(i, arg))
-    
-  if __name__ == "__main__":
-    main()
-       
-  
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_slice = sys.argv[1:]
+    argv_len = len(arg_slice)
+    if argv_len == 0:
+        print("{} arguments.".format(argv_len))
+    elif argv_len == 1:
+        print("{} argument:".format(argv_len))
+        print("{}: {}".format(argv_len, arg_slice[0]))
+    else:
+        print("{} arguments:".format(argv_len))
+        for i in range(1, argv_len + 1):
+            print("{}: {}".format(i, sys.argv[i]))
