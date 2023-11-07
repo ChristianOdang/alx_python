c82d7f878d0fc46965a7ba571fec8616e0842bd2
Peter Johnson Odang <118651777+ChristianOdang@users.noreply.github.com>
2023-11-07 13:07:11 +0100

    Update 4-error_code.py

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,25 +1,13 @@
-""" 
-This is a Python script that takes URL and send a request to the url
-and display the response of the results.
+"""
+takes in a URL, sends a request to the URL 
+and displays the body of the response.
 """
 import sys
 import requests
 
-
-""" 
-This function send request using the url requests module
-
-Parameters:
-    URL: the link to the url to send request to
-    
-Return:
-    return: the text response
-    
-"""
-
 url = sys.argv[1]
 response = requests.get(url)
 if response.status_code >= 400:
-    print("Error code:", response.status_code)
+    print("Error code:",response.status_code)
 else:
-    print(response)
+    print(response.text)
